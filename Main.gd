b9e122cc7ce39457319ac4e5aa6954830ad0f82f
Phil Howlett <phil.g.howlett@gmail.com>
2020-06-18 16:09:39 +1000

    Added dump report function

--- Main.gd
+++ Main.gd
@@ -16,17 +16,17 @@ var emergency_flow := 0						# EF
 var primary_level := 120 					# PV
 var primary_quality := 1 					# QP
 var primary_damage := 0						# PD
-var primary_failure := 0	 				# PB
+var primary_failure := 0 					# PB
 var primary_flow := 0						# PF
 var primary_heat := 0						# PH
 var secondary_level := 120					# SV
-var secondary_quality := 1					# QS
+var secondary_quality := 1 					# QS
 var secondary_damage := 0					# SD
 var secondary_failure := 0					# SB
 var secondary_flow := 0						# SF
-var control_rods := 0						# A
-var control_rods_prev_1 := 0 				# A1
-var control_rods_prev_2 := 0 				# A2
+var control_rod_position := 0				# A
+var control_rod_position_prev_1 := 0 		# A1
+var control_rod_position_prev_2 := 0 		# A2
 var reactor_heat := 0						# RH
 var reactor_level := 0						# RL
 var reactor_temperature := 25				# RT
@@ -41,10 +41,13 @@ var tower_previous_temp := 25				# T9
 var generator_damage := 0					# GD
 var generator_failure := 0					# GB
 var generator_production := 0				# GZ
+var kilowatt_average := 0					# KW
+var kilowatt_value := 0						# VE
 var day_number := 0							# DAY
 var previous_day_number := 0 				# D4
 var maintenance_days := 0					# MD
-var total_damage := 0						# DMGE
+var total_damage := 0 						# DMGE
+var fuel_remaining := 100					# FL
 
 # -[ private variables ]---------------------------------------------------------------------------
 # -[ onready variables ]---------------------------------------------------------------------------
@@ -54,6 +57,8 @@ var total_damage := 0						# DMGE
 
 func _ready() -> void:
 	do_maintenance()
+
+	dump_report()
 	pass
 
 
@@ -113,6 +118,30 @@ func do_maintenance() -> void:
 
 	# reset current generator production
 	generator_production = 0
+
+
+func dump_report() -> void:
+	# Simply dumps a text based report to the console, very similar to the original
+	# basic application. This is to check things are working as intended
+	print("---------------------------------------------------")
+	print("Day number: ", day_number)
+	print("Control Rod Position: ", control_rod_position, "%")
+	print("\n")
+	print("Temperatures:  MAX   CHANGE    NOW")
+	print("Reactor        800        ", reactor_temperature - reactor_previous_temperature, "     ", reactor_temperature)
+	print("Heat exchange  500        ", exchange_temperature - exchange_previous_temperature, "     ", exchange_temperature)
+	print("Cooling tower  300        ", tower_temperature - tower_previous_temp, "     ", tower_temperature)
+	print("\n")
+	print("  Power output (Max 2000): ", generator_production, " kW")
+	print("     Average power output: ", kilowatt_average, " kW")
+	print("Value of energy produced : $ ", kilowatt_value)
+	print("\n")
+	print("Coolants:    LEVEL   LEAKAGE   FLOW")
+	print("Emergency      ", emergency_level, "    ", emergency_damage, "/day      ", emergency_flow)
+	print("Primary        ", primary_level, "    ", primary_damage, "/day      ", primary_flow)
+	print("Secondary      ", secondary_level, "    ", secondary_damage, "/day      ", secondary_flow)
+	print("% Fuel remaining = ", fuel_remaining)
+	print("---------------------------------------------------")
 
 
 # -[ private methods ]-----------------------------------------------------------------------------
